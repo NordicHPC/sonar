7deaa99f06a0419f6fbfd873f12e5e124ac1ab97
Radovan Bast <bast@users.noreply.github.com>
2019-01-17 17:03:13 +0100

    add GET endpoint /data/example/

--- sonar/web.py
+++ sonar/web.py
@@ -1,5 +1,15 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
+
+def _generate_example_data():
+    # FIXME: later we will read this from files
+    data = {}
+    data["Vasp"] = {"percent": 24.3,
+                    "subcalls": {"vasp_std": 43.6, "vasp.5.3.2": 16.8, "std": 15.6, "vasp.NGZhalf": 10.3, "vasp.5.3.5": 9.5}}
+    data["foo"] = {"percent": 24.3,
+                    "subcalls": {"vasp_std": 43.6, "vasp.5.3.2": 16.8, "std": 15.6, "vasp.NGZhalf": 10.3, "vasp.5.3.5": 9.5}}
+    return data
 
 
 def _get_subdir(dirname):
@@ -14,6 +24,10 @@ def main(args):
     @app.route('/')
     def index():
         return render_template('data_visualisation.html')
+
+    @app.route('/data/example/', methods=['GET'])
+    def get_example():
+        return jsonify({'software': _generate_example_data()})
 
     app.debug = args['debug']
     app.run(host=args['host'], port=args['port'])
