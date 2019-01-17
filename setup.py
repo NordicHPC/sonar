f70e3388f1636cd5161569b2e643c125167b3794
Radovan Bast <bast@users.noreply.github.com>
2019-01-17 16:04:28 +0100

    require flask

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="sonar",
     packages=find_packages(),
-    entry_points={'console_scripts': ['sonar = sonar.cli:main']}
+    entry_points={'console_scripts': ['sonar = sonar.cli:main']},
+    install_requires=[
+        'flask==1.0.2'
+    ],
 )
 
 #FIXME: example configs, mappings, etc. should be part of the installable package
