ce699beace868c7da63c9d074ff79920846327e2
Radovan Bast <bast@users.noreply.github.com>
2019-05-11 14:26:28 +0200

    set version to 0.1.0

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
+import os
 from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+version = {}
+with open(os.path.join('sonar', 'version.py')) as f:
+    exec(f.read(), version)
 
 setup(
     name="sonar",
+    version=version['__version__'],
+    description='Tool to profile usage of HPC resources by regularly probing processes using ps.',
+    url='https://github.com/uit-no/sonar',
+    maintainer='Radovan Bast',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     entry_points={'console_scripts': ['sonar = sonar.cli:main']},
     install_requires=[
         'flask==1.0.2'
     ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
-
-#FIXME: example configs, mappings, etc. should be part of the installable package
