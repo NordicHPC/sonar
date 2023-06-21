465927276225a81f703f6602e7c9aa622e647879
Lars T Hansen <larstha@uio.no>
2023-06-21 13:38:49 +0200

    For #53: Propagate errors from the command module, and implement some skeleton logging.
    The current error propagation leaks the error code from the command
    module up through the layers, even though the "get information" layer
    need not run any subcommands.  This can be fixed later if we start
    going directly to /proc for information.

--- src/command.rs
+++ src/command.rs
@@ -1,32 +1,79 @@
 use std::time::Duration;
-use subprocess::{Exec, Redirection};
+use subprocess::{Exec, ExitStatus, Redirection};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum CmdError {
+    CouldNotStart,
+    Failed,
+    Hung,
+    InternalError,
+}
 
 // the pipe is here as a workaround for https://github.com/rust-lang/rust/issues/45572
 // see also https://doc.rust-lang.org/std/process/index.html
-pub fn safe_command(command: &str, timeout_seconds: u64) -> Option<String> {
-    let mut p = Exec::shell(command)
+
+pub fn safe_command(command: &str, timeout_seconds: u64) -> Result<String, CmdError> {
+    let mut p = match Exec::shell(command)
         .stdout(Redirection::Pipe)
         .stderr(Redirection::Pipe)
         .popen()
-        .ok()?;
+    {
+        Ok(p) => p,
+        Err(_) => {
+            // TODO: Possibly too coarse-grained but the documentation is not
+            // helpful in clarifying what might have happened.
+            return Err(CmdError::CouldNotStart);
+        }
+    };
 
-    if (p.wait_timeout(Duration::new(timeout_seconds, 0)).ok()?).is_some() {
-        let (out, err) = p.communicate(None).ok()?;
-
-        match err {
-            Some(e) => {
-                if e.is_empty() {
-                    out
+    match p.wait_timeout(Duration::new(timeout_seconds, 0)) {
+        Ok(Some(ExitStatus::Exited(0))) => match p.communicate(None) {
+            Ok((Some(stdout), Some(stderr))) => {
+                if stderr.is_empty() {
+                    Ok(stdout)
                 } else {
-                    None
+                    Err(CmdError::Failed)
                 }
             }
-            None => out,
+            Ok((_, _)) => Err(CmdError::InternalError),
+            Err(_) => Err(CmdError::Failed),
+        },
+        Ok(Some(ExitStatus::Exited(126))) => {
+            // 126 == "Command cannot execute"
+            Err(CmdError::CouldNotStart)
         }
-    } else {
-        p.kill().ok()?;
-        p.wait().ok()?;
-
-        None
+        Ok(Some(ExitStatus::Exited(127))) => {
+            // 127 == "Command not found"
+            Err(CmdError::CouldNotStart)
+        }
+        Ok(Some(_)) => Err(CmdError::Failed),
+        Ok(None) => {
+            // Timeout
+            if p.kill().is_ok() && p.wait().is_ok() {
+                Err(CmdError::Hung)
+            } else {
+                Err(CmdError::InternalError)
+            }
+        }
+        Err(_) => Err(CmdError::InternalError),
     }
+}
+
+#[test]
+fn test_safe_command() {
+    // Should work, because we should be running this in the repo root.
+    match safe_command("ls Cargo.toml", 2) {
+	Ok(_) => {},
+	Err(_) => assert!(false)
+    }
+    // This really needs to be the output
+    assert!(safe_command("grep '^name =' Cargo.toml", 2) == Ok("name = \"sonar\"\n".to_string()));
+    // Not found
+    assert!(safe_command("no-such-command-we-hope", 2) == Err(CmdError::CouldNotStart));
+    // Wrong permissions, not executable
+    assert!(safe_command("/etc/passwd", 2) == Err(CmdError::CouldNotStart));
+    // Should take too long
+    assert!(safe_command("traceroute amazon.com", 2) == Err(CmdError::Hung));
+    // Exited with error
+    assert!(safe_command("ls /abracadabra", 2) == Err(CmdError::Failed));
 }
