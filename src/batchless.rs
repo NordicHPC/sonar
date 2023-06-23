49bd826c8030af56faa1d57cdb88b9436ad815d5
Lars T Hansen <larstha@uio.no>
2023-06-23 11:58:00 +0200

    fixup! Fix #58 - Batchless jobs - synthesize job ID from process tree.

--- src/batchless.rs
+++ src/batchless.rs
@@ -2,24 +2,21 @@
 //
 // In this system, the job ID of a process is found by walking the tree of jobs from the PID until
 // we reach a process that is directly below a session leader, and then taking that process's PID as
-// the job ID.  (The session leader is a process whose session PID is its own pid.)  If the process
-// we're given is a session leader then we take its own PID to be the job ID.  Everything else ends
-// up at the init process, which is probably OK.
+// the job ID.  (The session leader is a process whose session PID is its own PID.)  If the process
+// we're given is a session leader then we take its own PID to be the job ID.  Most other things end
+// up at the init process or at "the system", which is OK - their own PIDs are their job IDs.
 //
-// There's a remote possibility that the job ID will be reused during the lifetime of the system,
-// confusing our statistics.  On Linux, the PIDs wrap around at about 4e6.  However, the job ID is
-// not used in isolation (at the moment), but always with the user and the command name.  Should we
-// want to add even more information we could incorporate eg the PID of the session leader, and/or
-// the boot time of the system.
+// There's a possibility that the job ID will be reused during the lifetime of the system, confusing
+// our statistics.  On Linux, the PIDs wrap around at about 4e6, and on a busy system this happens
+// in a matter of days.  However, the job ID is not used in isolation (at the moment), but always
+// with the user and the command name, so the reuse problem is not huge.
 //
 // There's also a challenge with this scheme in that, since the output is keyed on program name as
 // well as on user name and job ID, multiple output lines are going to have the same user name and
-// job ID in a tree of *different* processes (ie where subprocesses of the root process exec
-// something else).  This is not wrong but it is something that the consumer must take into account.
-// For example, in assessing the resources for a job, the resources for all the different programs
-// for the job must be taken into account.
-//
-// (Also this puts some more pressure on the reused-PID problem.)
+// job ID in a tree of *different* processes (ie where subprocesses of the root process in the job
+// exec something with a different name).  This is not wrong but it is something that the consumer
+// must take into account.  For example, in assessing the resources for a job, the resources for all
+// the different programs for the job must be taken into account.
 
 use crate::jobs;
 #[cfg(test)]
@@ -29,7 +26,7 @@ use std::collections::HashMap;
 
 pub struct BatchlessJobManager {
     // Process tables can be large and searching them sequentially for every lookup will be slow, so
-    // add a cache.  Various structures could work.  Here a hashmap maps pid -> (session, ppid).
+    // add a cache.  Various structures could work.  Here a hashmap maps PID -> (session, PPID).
     cache: HashMap<usize, (usize, usize)>,
 }
 
@@ -42,20 +39,18 @@ impl BatchlessJobManager {
 }
 
 impl BatchlessJobManager {
-    fn lookup(&mut self, processes: &[process::Process], pid: usize) -> Option<(usize, usize)> {
-        let probe = self.cache.get(&pid);
+    fn lookup(&mut self, processes: &[process::Process], want_pid: usize) -> Option<(usize, usize)> {
+        let probe = self.cache.get(&want_pid);
         if probe.is_some() {
             return probe.copied();
         }
 
-        let mut i = 0;
-        while i < processes.len() {
-            if processes[i].pid == pid {
-                let entry = (processes[i].session, processes[i].ppid);
-                self.cache.insert(pid, entry);
+        for process::Process { pid, ppid, session, .. } in processes {
+            if *pid == want_pid {
+                let entry = (*session, *ppid);
+                self.cache.insert(want_pid, entry);
                 return Some(entry);
             }
-            i += 1
         }
 
         None
