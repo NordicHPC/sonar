9dcfb417592faa43b9f0d8acb201d7c155af2b67
Lars T Hansen <larstha@uio.no>
2023-08-11 11:15:23 +0200

    Implement rollup by job id and command

--- src/ps.rs
+++ src/ps.rs
@@ -32,10 +32,12 @@ type JobID = usize;
 // of this data, internal or external to the program, may treat processes with job ID "0" as part of
 // the same job.
 
+#[derive(Clone)]
 struct ProcInfo<'a> {
     user: &'a str,
     command: &'a str,
     _pid: Pid,
+    rolledup: usize,
     job_id: usize,
     cpu_percentage: f64,
     cputime_sec: usize,
@@ -88,6 +90,7 @@ where
             user,
             command,
             _pid: pid,
+            rolledup: 0,
             job_id: lookup_job_by_pid(pid),
             cpu_percentage,
             cputime_sec,
@@ -102,12 +105,10 @@ where
 
 pub fn create_snapshot(
     jobs: &mut dyn jobs::JobManager,
+    rollup_by_jobid_and_command: bool,
     cpu_cutoff_percent: f64,
     mem_cutoff_percent: f64,
 ) {
-    let timestamp = time_iso8601();
-    let hostname = hostname::get().unwrap().into_string().unwrap();
-    let num_cores = num_cpus::get();
     let no_gpus = make_gpuset(None);
     let mut proc_by_pid = ProcTable::new();
 
@@ -197,43 +198,123 @@ pub fn create_snapshot(
 
     let mut writer = Writer::from_writer(io::stdout());
 
+    let timestamp = time_iso8601();
+    let hostname = hostname::get().unwrap().into_string().unwrap();
+    let num_cores = num_cpus::get();
     const VERSION: &str = env!("CARGO_PKG_VERSION");
+    let print_params = PrintParameters {
+        hostname: &hostname,
+        timestamp: &timestamp,
+        num_cores,
+        version: VERSION,
+        cpu_cutoff_percent,
+        mem_cutoff_percent
+    };
 
-    for (_, proc_info) in proc_by_pid {
-        if (proc_info.cpu_percentage < cpu_cutoff_percent) && (proc_info.mem_percentage < mem_cutoff_percent) {
-            continue;
+    if rollup_by_jobid_and_command {
+        // This is a little complicated because jobs with job_id 0 cannot be rolled up.
+        //
+        // - There is an array `rolledup` of ProcInfo nodes that represent rolled-up data
+        //
+        // - When the job ID of a job in `proc_by_pid` is zero, the entry in `rolledup` is a copy of
+        //   that job; these jobs cannot be rolled up (this is why it's complicated)
+        //
+        // - Otherwise, the entry in `rolledup` represent rolled-up information for a (job, command) pair
+        //
+        // - There is a hash table `index` that maps the (job, command) pair to the entry in `rolledup`,
+        //   if any
+        //
+        // - When we're done rolling up, we print the `rolledup` table.
+        //
+        // Filtering is performed after rolling up, so if a rolled-up job has a bunch of dinky
+        // processes that together push it over the filtering limit then it will be printed.  This
+        // is probably the right thing.
+
+        let mut rolledup = vec![];
+        let mut index = HashMap::<(JobID, &str), usize>::new();
+        for (_, proc_info) in &proc_by_pid {
+            if proc_info.job_id == 0 {
+                rolledup.push(proc_info.clone());
+            } else {
+                let key = (proc_info.job_id, proc_info.command);
+                if let Some(x) = index.get(&key) {
+                    let p = &mut rolledup[*x];
+                    p.cpu_percentage += proc_info.cpu_percentage;
+                    p.cputime_sec += proc_info.cputime_sec;
+                    p.mem_percentage += proc_info.mem_percentage;
+                    p.mem_size_kib += proc_info.mem_size_kib;
+                    p.gpu_cards.extend(&proc_info.gpu_cards);
+                    p.gpu_percentage += proc_info.cpu_percentage;
+                    p.gpu_mem_percentage += proc_info.gpu_mem_percentage;
+                    p.gpu_mem_size_kib += proc_info.gpu_mem_size_kib;
+                    p.rolledup += 1;
+                } else {
+                    let x = rolledup.len();
+                    index.insert(key, x);
+                    rolledup.push(proc_info.clone());
+                    // We do not increment the clone's `rolledup` counter here because that counter
+                    // counts how many *other* records have been rolled into the canonical one, 0
+                    // means "no interesting information" and need not be printed.
+                }
+            }
         }
-        // "unknown" is not implemented, see https://github.com/NordicHPC/sonar/issues/75
-        let mut gpus_comma_separated: String = proc_info
-            .gpu_cards
-            .iter()
-            .map(|&num| num.to_string())
-            .collect::<Vec<String>>()
-            .join(",");
 
-        if gpus_comma_separated.is_empty() {
-            gpus_comma_separated = "none".to_string();
+        for r in rolledup {
+            print_record(&mut writer, &print_params, &r); 
         }
-
-        writer
-            .write_record([
-                &format!("v={VERSION}"),
-                &format!("time={timestamp}"),
-                &format!("host={hostname}"),
-                &format!("cores={num_cores}"),
-                &format!("user={}", proc_info.user),
-                &format!("job={}", proc_info.job_id),
-                &format!("cmd={}", proc_info.command),
-                &format!("cpu%={}", three_places(proc_info.cpu_percentage)),
-                &format!("cpukib={}", proc_info.mem_size_kib),
-                &format!("gpus={}", gpus_comma_separated),
-                &format!("gpu%={}", three_places(proc_info.gpu_percentage)),
-                &format!("gpumem%={}", three_places(proc_info.gpu_mem_percentage)),
-                &format!("gpukib={}", proc_info.gpu_mem_size_kib),
-                &format!("cputime_sec={}", proc_info.cputime_sec),
-            ])
-            .unwrap();
+    } else {
+        for (_, proc_info) in proc_by_pid {
+            print_record(&mut writer, &print_params, &proc_info);
+        }
     }
 
     writer.flush().unwrap();
+}
+
+struct PrintParameters<'a> {
+    hostname: &'a str,
+    timestamp: &'a str,
+    num_cores: usize,
+    version: &'a str,
+    cpu_cutoff_percent: f64,
+    mem_cutoff_percent: f64,
+}
+
+fn print_record<W: io::Write>(writer: &mut Writer<W>, params: &PrintParameters, proc_info: &ProcInfo) {
+    if (proc_info.cpu_percentage < params.cpu_cutoff_percent) &&
+        (proc_info.mem_percentage < params.mem_cutoff_percent) {
+        return;
+    }
+
+    // "unknown" is not implemented, see https://github.com/NordicHPC/sonar/issues/75
+    let mut gpus_comma_separated: String = proc_info
+        .gpu_cards
+        .iter()
+        .map(|&num| num.to_string())
+        .collect::<Vec<String>>()
+        .join(",");
+
+    if gpus_comma_separated.is_empty() {
+        gpus_comma_separated = "none".to_string();
+    }
+
+    writer
+        .write_record([
+            &format!("v={}", params.version),
+            &format!("time={}", params.timestamp),
+            &format!("host={}", params.hostname),
+            &format!("cores={}", params.num_cores),
+            &format!("user={}", proc_info.user),
+            &format!("job={}", proc_info.job_id),
+            &format!("cmd={}", proc_info.command),
+            &format!("cpu%={}", three_places(proc_info.cpu_percentage)),
+            &format!("cpukib={}", proc_info.mem_size_kib),
+            &format!("gpus={}", gpus_comma_separated),
+            &format!("gpu%={}", three_places(proc_info.gpu_percentage)),
+            &format!("gpumem%={}", three_places(proc_info.gpu_mem_percentage)),
+            &format!("gpukib={}", proc_info.gpu_mem_size_kib),
+            &format!("cputime_sec={}", proc_info.cputime_sec),
+            &format!("rolledup={}", proc_info.rolledup),
+        ])
+        .unwrap();
 }
