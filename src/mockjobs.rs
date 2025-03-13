911f9131638da424bd0126a17ef242749834f27b
Lars T Hansen <larstha@uio.no>
2025-03-13 13:00:39 +0100

    Fix #182 - remove --batchless, emit jobid epoch

--- src/mockjobs.rs
+++ src/mockjobs.rs
@@ -1,12 +1,18 @@
 use crate::jobsapi;
 use crate::procfs;
+use crate::systemapi;
 
 use std::collections::HashMap;
 
 pub struct MockJobManager {}
 
 impl jobsapi::JobManager for MockJobManager {
-    fn job_id_from_pid(&self, pid: usize, _processes: &HashMap<usize, procfs::Process>) -> usize {
-        pid
+    fn job_id_from_pid(
+        &self,
+        _system: &dyn systemapi::SystemAPI,
+        pid: usize,
+        _processes: &HashMap<usize, procfs::Process>,
+    ) -> (usize,bool) {
+        (pid, false)
     }
 }
--- src/slurm.rs
+++ src/slurm.rs
@@ -1,44 +1,70 @@
-// Abstraction of jobsapi::JobManager for SLURM.
+use crate::systemapi;
+#[cfg(test)]
+use crate::mocksystem;
 
-use crate::jobsapi;
-use crate::procfs;
-
+#[cfg(test)]
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
 
-pub struct SlurmJobManager {}
+// This returns Some(n) where n > 0 if we could parse the job ID, Some(0) if the overall pattern
+// matched but the ID was not parseable, or None otherwise.  Thus None is a signal to fall back to
+// other (non-Slurm) mechanisms.
 
-impl jobsapi::JobManager for SlurmJobManager {
-    fn job_id_from_pid(&self, pid: usize, _processes: &HashMap<usize, procfs::Process>) -> usize {
-        let slurm_job_id = get_slurm_job_id(pid).unwrap_or_default();
-        slurm_job_id.trim().parse::<usize>().unwrap_or_default()
-    }
-}
-
-fn get_slurm_job_id(pid: usize) -> Option<String> {
-    match File::open(format!("/proc/{pid}/cgroup")) {
-        Ok(f) => {
+pub fn get_job_id(system: &dyn systemapi::SystemAPI, pid: usize) -> Option<usize> {
+    match system.get_procfs().read_to_string(&format!("{pid}/cgroup")) {
+        Ok(text) => {
             // We want \1 of the first line that matches "/job_(.*?)/"
             //
-            // The reason is that there are several lines in that file that look roughly like this,
-            // with different contents (except for the job info) but with the pattern the same:
+            // The reason is that there can be several lines in that file that look roughly like
+            // this, with different contents (except for the job info) but with the pattern the
+            // same:
             //
             //    10:devices:/slurm/uid_2101171/job_280678/step_interactive/task_0
 
-            for l in BufReader::new(f).lines() {
-                if let Ok(l) = l {
-                    if let Some(x) = l.find("/job_") {
-                        if let Some(y) = l[x + 5..].find('/') {
-                            return Some(l[x + 5..x + 5 + y].to_string());
-                        }
+            for l in text.split('\n') {
+                if let Some(x) = l.find("/job_") {
+                    if let Some(y) = l[x + 5..].find('/') {
+                        // Pattern found, so commit
+                        return match l[x + 5..x + 5 + y].trim().parse::<usize>() {
+                            Ok(n) => Some(n),
+                            _ => Some(0),
+                        };
                     }
-                } else {
-                    return None;
                 }
             }
+            // Readable lines exhausted
             None
         }
         Err(_) => None,
     }
+}
+
+#[test]
+fn test_get_job_id() {
+    let mut files = HashMap::new();
+    // This first test case from a Fox compute node on 2025-03-13
+    files.insert("1337/cgroup".to_string(),
+                 "0::/system.slice/slurmstepd.scope/job_1392969/step_0/user/task_0\n".to_string());
+    // This second case from some older data
+    files.insert("1336/cgroup".to_string(),
+                 "10:devices:/slurm/uid_2101171/job_280678/step_interactive/task_0\n".to_string());
+    files.insert("1338/cgroup".to_string(),
+                 "random garbage\n".to_string());
+    files.insert("1339/cgroup".to_string(),
+                 "/job_hello/\n".to_string());
+    let system = mocksystem::MockSystem::new().with_files(files).freeze();
+
+    let r = get_job_id(&system, 1337);
+    assert!(r.is_some());
+    assert!(r.unwrap() == 1392969);
+
+    let r = get_job_id(&system, 1336);
+    assert!(r.is_some());
+    assert!(r.unwrap() == 280678);
+
+    let r = get_job_id(&system, 1338);
+    assert!(r.is_none());
+
+    let r = get_job_id(&system, 1339);
+    assert!(r.is_some());
+    assert!(r.unwrap() == 0);
 }
