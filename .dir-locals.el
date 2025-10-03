129716ad0c3de2722ef4267877d8258371b08313
Lars T Hansen <larstha@uio.no>
2025-10-03 05:26:12 +0000

    No bug: More indentation for C code

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;; This is meant to approximate what we find in clang-format.txt.
+
+((c-mode . ((c-mode . "stroustrup")
+	    (indent-tabs-mode . nil)
+	    (fill-column . 100)
+	    (c-file-offsets .
+	     ((case-label . 0)
+	      (statement-case-intro . 4))))))
