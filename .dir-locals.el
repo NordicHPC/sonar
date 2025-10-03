d29c5bcef0c8962f3ae73a3a2733c099da6407df
Lars T Hansen <larstha@uio.no>
2025-10-03 05:56:51 +0000

    Actually test the formatting settings

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,18 @@
 ;; This is meant to approximate what we find in clang-format.txt.
+;; This is supposed to be "stroustrup + simple tweaks".
 
-((c-mode . ((c-default-style . "stroustrup")
-	    (indent-tabs-mode . nil)
+((c-mode . ((indent-tabs-mode . nil)
 	    (fill-column . 100)
+	    (c-basic-offset . 4)
+	    (c-comment-only-line-offset . 0)
 	    (c-file-offsets .
 	     ((case-label . 0)
-	      (statement-block-intro . 4))))))
+	      (statement-case-intro . 4)
+	      (statement-block-intro . +)
+	      (substatement-open . 0)
+	      (substatement-label . 0)
+	      (label . 0)
+	      (brace-list-intro first
+				c-lineup-2nd-brace-entry-in-arglist
+				c-lineup-class-decl-init-+ +)
+	      (statement-cont . +))))))
