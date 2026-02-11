1a2964789c95d34e1800dabe6a4054617d5c4134
Lars T Hansen <larstha@uio.no>
2026-02-11 13:08:02 +0100

    Fix #496 - better name for http proxy setting

--- src/datasink/kafka.rs
+++ src/datasink/kafka.rs
@@ -102,7 +102,7 @@ impl KafkaSink {
                 })
             } else {
                 let rest_endpoint = kafka.rest_endpoint.clone();
-                let rest_proxy = kafka.rest_proxy.clone();
+                let http_proxy = kafka.http_proxy.clone();
                 let curl_cmd = if let Some(ref curl) = ini.programs.curl_cmd {
                     curl.clone()
                 } else {
@@ -112,7 +112,7 @@ impl KafkaSink {
                     http_producer(
                         &curl_cmd,
                         &rest_endpoint,
-                        &rest_proxy,
+                        &http_proxy,
                         &client_id,
                         sasl_identity,
                         sending_window,
@@ -420,7 +420,7 @@ fn make_sender_adapter(
 fn http_producer(
     cmd: &str,
     api_endpoint: &str,
-    proxy_address: &str,
+    http_proxy: &str,
     client_id: &str,
     sasl_identity: Option<(String, String)>,
     sending_window: u64,
@@ -457,7 +457,7 @@ fn http_producer(
                 armed = false;
                 // Note, the /Sending {} items/ pattern is used by regression tests.
                 log::debug!("Sending window open.  Sending {} items", backlog.len());
-                http_send_messages(cmd, api_endpoint, proxy_address, client_id, retry_count, &sasl_identity, &control_and_errors, backlog);
+                http_send_messages(cmd, api_endpoint, http_proxy, client_id, retry_count, &sasl_identity, &control_and_errors, backlog);
                 backlog = vec![];
             }
             recv(incoming_message_queue) -> msg => match msg {
@@ -466,7 +466,7 @@ fn http_producer(
                     must_arm = !armed;
                 }
                 Ok(Message::Stop) | Err(_) => {
-                    http_send_messages(cmd, api_endpoint, proxy_address, client_id, retry_count, &sasl_identity, &control_and_errors, backlog);
+                    http_send_messages(cmd, api_endpoint, http_proxy, client_id, retry_count, &sasl_identity, &control_and_errors, backlog);
                     break 'producer_loop;
                 }
             }
@@ -483,7 +483,7 @@ fn http_producer(
 fn http_send_messages(
     cmd: &str,
     api_endpoint: &str,
-    proxy_address: &str,
+    http_proxy: &str,
     client_id: &str,
     retry_count: i32,
     sasl_identity: &Option<(String, String)>,
@@ -511,9 +511,9 @@ fn http_send_messages(
         .stdin(std::process::Stdio::piped())
         .stdout(std::process::Stdio::piped())
         .stderr(std::process::Stdio::piped());
-    if proxy_address != "" {
-        cmd.env("http_proxy", proxy_address)
-            .env("https_proxy", proxy_address);
+    if http_proxy != "" {
+        cmd.env("http_proxy", http_proxy)
+            .env("https_proxy", http_proxy);
     }
     match cmd.spawn() {
         Ok(mut child) => {
